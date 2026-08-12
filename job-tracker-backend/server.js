835af32f96ce724481c1d2de2931887486aeeb13
Radio6 <radinajorlou6@gmail.com>
2026-08-12 19:34:02 -0400

    new commit

--- job-tracker-backend/server.js
+++ job-tracker-backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 app.get('/', (req, res) => {
-  res.send('Server is running!');
+  res.send('Server is running! hellooooooo');
 });
 
 app.get('/applications', async (req, res) => {
